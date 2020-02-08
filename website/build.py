f723f6b0149e6ba7db0d440251ee1f6b3e03bdb8
Josh Gruenstein <joshuagruenstein@gmail.com>
2020-02-08 12:19:17 -0500

    watch mode for build

--- website/build.py
+++ website/build.py
@@ -1,5 +1,5 @@
 from jemdoc import *
-import os, shutil
+import os, shutil, sys
 
 CONF_FILE = 'mysite.conf'
 OUT_DIR = '../docs'
@@ -20,25 +20,47 @@ def make_file(filename):
     )
     
     procfile(f)
-
-if os.path.exists(OUT_DIR):
-    shutil.rmtree(OUT_DIR) 
-
-os.makedirs(OUT_DIR)
-
-
-for (path, dirs, files) in os.walk('.'):
-    # don't traverse hidden directories or files
-    dirs[:] = [d for d in dirs if not
-        (d[0] == '.' or d == 'build')
-    ]
     
-    files = [f for f in files if not f[0] == '.']
- 
-    for f in files:
-        file = os.path.join(path, f)
+def get_watch_files():
+    if os.path.exists(OUT_DIR):
+        shutil.rmtree(OUT_DIR) 
+
+    os.makedirs(OUT_DIR)
+    watch_files = []
+
+    for (path, dirs, files) in os.walk('.'):
+        # don't traverse hidden directories or files
+        dirs[:] = [d for d in dirs if not
+            (d[0] == '.' or d == 'build')
+        ]
+
+        watch_files += [
+            os.path.join(path, f) for f in files
+            if not f[0] == '.'
+        ]
+    
+    return watch_files
+
+def build(watch_files):
+    for file in watch_files:
         if file.endswith('.jemdoc'):
             make_file(file[:-7])
         elif file.endswith('.css'):
-            shutil.copyfile(file, os.path.join(OUT_DIR,file))
- 
+            shutil.copyfile(
+                file, os.path.join(OUT_DIR,file)
+            )
+
+watch_files = {
+    f: os.stat(f).st_mtime
+    for f in get_watch_files()
+}
+
+build(watch_files.keys())
+
+while sys.argv[-1] == '--watch':
+    for file, time in watch_files.items():
+        if os.stat(file).st_mtime > time:
+            watch_files[file] = os.stat(file).st_mtime
+            print('Detected change in', file, 'so rebuilding')
+            build(watch_files.keys())
+            break
